c6d74653316c1cb964f8bffe7a13f3d8173f4d66
Vignesh <vignesh@visionbox.ai>
2021-11-10 19:15:50 +0530

    clean unwanted files

--- dataset/load_dataset.py
+++ dataset/load_dataset.py
@@ -3,20 +3,37 @@ import tensorflow as tf
 import numpy as np
 from build_siamese_pairs import *
 import config
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers.experimental import preprocessing
 from utils import list_images
-import os
+
 def load_images(input,channels=3,shape=(224,224)):
 	imageA = tf.io.read_file(input[0])
 	imageA = tf.image.decode_image(imageA, channels=channels)
-	imageA = tf.image.resize(imageA,shape) / 255.0
+	imageA = tf.image.resize(imageA,shape)
 	imageB = tf.io.read_file(input[1])
 	imageB = tf.image.decode_image(imageB, channels=channels)
-	imageB = tf.image.resize(imageB,shape) / 255.0
+	imageB = tf.image.resize(imageB,shape)
 	encodedLabel = input[2]
 	# return the image and the integer encoded label
 	return (imageA,imageB,encodedLabel)
 
+
+trainAug = Sequential([
+	preprocessing.Rescaling(scale=1.0 / 255),
+	preprocessing.RandomFlip("horizontal_and_vertical"),
+	preprocessing.RandomZoom(
+		height_factor=(-0.05, -0.15),
+		width_factor=(-0.05, -0.15)),
+	preprocessing.RandomRotation(0.3)
+])
+
+testAug = Sequential([
+	preprocessing.Rescaling(scale=1.0 / 255)
+])
+
 def augment(imageA,imageB,label):
+
 	return (imageA,imageB,label)
 
 
@@ -40,7 +57,10 @@ assert len(test_paths) == len(testlabels)
 trainX,trainY,dictmap=make_random_pairs(train_paths,trainlabels,labelsmap=True)
 valX,valY,_=make_random_pairs(val_paths,vallabels,labelsmap=True)
 testX,testY,_=make_random_pairs(test_paths,testlabels,labelsmap=True)
+
+
 print("[INFO] creating a tf.data input pipeline..")
+
 #build train data pipeline
 train_input=np.concatenate((trainX, trainY), axis=1)
 trainDS = tf.data.Dataset.from_tensor_slices(train_input)
